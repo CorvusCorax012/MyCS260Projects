87a38f3f04d4115205a9f843144dd85f0e865d4e
Strider <graysonschmalz@gmail.com>
2022-02-19 16:35:23 -0700

    added about and made api call page pretty

--- homePage2.0/menuScript.js
+++ homePage2.0/menuScript.js
@@ -1,3 +1,5 @@
+"use strict";
+
 $(document).ready(function () {
   var trigger = $('.hamburger'),
       overlay = $('.overlay'),
@@ -21,8 +23,36 @@ $(document).ready(function () {
         isClosed = true;
       }
   }
-  
+
   $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
   });  
+
+  
+  fetch("https://type.fit/api/quotes")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    var button = $('.quoteButton')
+    button.click(function () {
+      getQuote(data);      
+    });
+    getQuote(data);
+  });
+
+  function getQuote(data) {
+    let quoteArray = data.map((n) => n.text);
+    let authorArray = data.map((n) => n.author);
+    let randNum = Math.floor(Math.random() * quoteArray.length)
+    const quote = quoteArray[randNum];
+    const author = authorArray[randNum];
+    if(author === null) {
+      author = "Unknown";
+    }
+    document.getElementById("quote").innerHTML = "\"" + quote + "\"";
+    document.getElementById("author").innerHTML = "-" + author;
+  }
+
 });
